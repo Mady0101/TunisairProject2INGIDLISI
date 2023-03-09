b8c82ef25595f92e5702583acfc263eaeaae4474
Mady0101 <mrabetmohamedmahdi@gmail.com>
2023-03-09 14:24:50 +0100

    init project

---
+++ src/main/java/com/example/MSSZtunisair2INGIDL1/MssZtunisair2Ingidl1Application.java
@@ -0,0 +1,13 @@
+package com.example.MSSZtunisair2INGIDL1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssZtunisair2Ingidl1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssZtunisair2Ingidl1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MSSZtunisair2INGIDL1/MssZtunisair2Ingidl1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSSZtunisair2INGIDL1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssZtunisair2Ingidl1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
